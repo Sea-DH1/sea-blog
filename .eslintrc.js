2ad45fa470690cb4a2caaff0b82e1c5a224dfb65
ldh <627247084@qq.com>
2021-04-26 00:57:29 +0800

    配置项目开发环境

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,45 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
   extends: [
     '@nuxtjs',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
+    'plugin:nuxt/recommended'
   ],
   plugins: [],
+  globals: {
+    require: true
+  },
   // add your custom rules here
-  rules: {},
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    // 'no-console': 0, //禁用 console
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-unused-vars': 1, // 禁止出现未使用过的变量
+
+    'prettier/prettier': [
+      'error',
+      {
+        tabWidth: 2,
+        useTabs: false,
+        bracketSpacing: true,
+        singleQuote: true,
+        trailingComma: 'none',
+        jsxBracketSameLine: true,
+        semi: false,
+        endOfLine: 'auto'
+        // printWidth: 50
+        //  "endOfLine": "auto" //不同系统换行符不一致
+      }
+    ]
+  }
 }
---
+++ components/framework/app-layout/app-layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>123</div>
+</template>
+
+<script>
+export default {
+  name: 'AppLayout',
+  layout: 'default',
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss"></style>
